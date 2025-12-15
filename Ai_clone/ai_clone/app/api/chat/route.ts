586d8d84e4571bc9f9df7fc036e519055907bcbe
prince476-web <princemovaliya467@gmail.com>
2025-12-15 21:20:44 +0530

    ai_clone done

--- Ai_clone/ai_clone/app/api/chat/route.ts
+++ Ai_clone/ai_clone/app/api/chat/route.ts
@@ -6,7 +6,7 @@ export const maxDuration = 30;
 export async function POST(req: Request) {
   const { messages } = await req.json();
   const result = streamText({
-    model: google("gemini-2.0-flash"),
+    model: google("gemini-2.5-flash"),
     messages: convertToModelMessages(messages),
   });
   return result.toUIMessageStreamResponse();
